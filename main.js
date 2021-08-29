9de28e243599640cf5ba03927d358f216ad54104
bt7tri <seong.il.kim.812@gmail.com>
2021-08-29 19:27:05 +0900

    JS: navbar menu click to scroll

--- main.js
+++ main.js
@@ -20,3 +20,21 @@ document.addEventListener('scroll', () =>{
         }
 
 });
+
+// Handle scrolling when tapping on navbar menu.
+const navbarMenu=document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event) =>{
+    
+    const target=event.target;
+    const link=target.dataset.link;
+    if (link==null)
+    {
+        return;
+    }
+    
+    //console.log(event.target.dataset.link);
+    const scrollTo=document.querySelector(link);
+    scrollTo.scrollIntoView({behavior: "smooth"});
+    //the name of "data-link" should be the same with the id of target section, and # should be added in front of the name.
+    
+});
