2d423ec185ebc9ef78f71e6263181385bbc88e29
bt7tri <seong.il.kim.812@gmail.com>
2021-08-30 20:02:20 +0900

    JS: Home-Transparent

--- main.js
+++ main.js
@@ -52,6 +52,18 @@ btnContact.addEventListener('click', (event) =>{
     scrollIntoViews('#contact');
 });
 
+// Make home slowly fade to transparent as the window scrolls down.
+const home=document.querySelector('.home__container');
+const homeHeight=home.getBoundingClientRect().height;
+document.addEventListener('scroll', ()=>{
+    //console.log(homeHeight);
+    //console.log(1-window.scrollY/homeHeight);
+    home.style.opacity=1-window.scrollY/homeHeight;
+});
+
+
+
+
 function scrollIntoViews(selector){
     const scrollTarget=document.querySelector(selector);
     scrollTarget.scrollIntoView({behavior: "smooth"});
